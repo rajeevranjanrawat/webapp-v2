b131bb2bcce905b92e1c6180fea897108121de7d
rajeevranjanrawat <40881447+rajeevranjanrawat@users.noreply.github.com>
2018-07-26 23:58:40 +0530

    update settings

--- webApp/settings.py
+++ webApp/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
 
@@ -135,5 +135,5 @@ STATICFILES_DIRS = (
     'static/'
 )
 
-MEDIA_URL = '/static/mediafiles/'
+MEDIA_URL = 'static/mediafiles/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static/mediafiles")
