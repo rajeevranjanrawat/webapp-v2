94f1ab387d71b054017ae0540740159171a85bdb
rajeevranjanrawat <40881447+rajeevranjanrawat@users.noreply.github.com>
2018-07-27 10:32:58 +0530

    Update settings.py

--- webApp/settings.py
+++ webApp/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'webApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['static/'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
