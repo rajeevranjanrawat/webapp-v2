236fe3e78c75a50f5bdcd372b5bc3cc5c0b39c43
rajeevranjanrawat <40881447+rajeevranjanrawat@users.noreply.github.com>
2018-08-03 16:49:33 +0530

    Update settings.py

--- webApp/settings.py
+++ webApp/settings.py
@@ -143,8 +143,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATIC_URL = 'static/'
-MEDIA_URL = 'static/mediafiles/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static/mediafiles")
+
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'whitenoise.middleware.WhiteNoiseMiddleware',
