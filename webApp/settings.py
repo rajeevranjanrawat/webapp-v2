38baaad1d59d024ee9cb12c87ce57e9883020247
rajeevranjanrawat <40881447+rajeevranjanrawat@users.noreply.github.com>
2018-08-03 18:25:01 +0530

    Update settings.py

--- webApp/settings.py
+++ webApp/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = (
 
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
